52d5f46391e09100c5701709a3e4011dd1d201e7
김하림 <kittyyakki93@gmail.com>
2026-04-05 14:22:56 +0900

    feat:(create IngredientRepository)

--- src/repository/ingredient/ingredient.repository.ts
+++ src/repository/ingredient/ingredient.repository.ts
@@ -3,10 +3,23 @@ import { PrismaService } from "src/service/prisma/prisma.service";
 export class IngredientRepository {
   constructor(private readonly prisma: PrismaService) { }
 
+  // 1. 간 영향 조회
   async findLiverImpacts(ids: number[]) {
     return this.prisma.ingredientLiverImpact.findMany({
       where: {
         ingredient_id: {in: ids},
+      }
+    })
+  }
+
+  // 2. interaction 상호작용 조회
+  async findInteractions(ids: number[]) {
+    return this.prisma.interaction.findMany({
+      where: {
+        OR: [
+          { ingredient_a_id: { in: ids } },
+          {ingredient_b_id: {in: ids}},
+        ]
       }
     })
   }
