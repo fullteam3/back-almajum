b9d31c109c13eb6dcc63a5ceb2965a3e800d50ba
김하림 <kittyyakki93@gmail.com>
2026-04-05 16:41:59 +0900

    feat:( fix InteractionController )

--- src/controller/interaction/interaction.controller.ts
+++ src/controller/interaction/interaction.controller.ts
@@ -1,9 +1,10 @@
 import { Body, Controller, Post } from "@nestjs/common";
-import { CreateInteractionDto } from "src/domain/interaction/dto/interaction.dto";
+import { CreateInteractionDto } from "src/domain/interaction/dto/create-interaction.dto.ts";
+
 import { InteractionService } from "src/service/interaction/interaction.service";
 
 @Controller('interaction')
-export class InteractionConteroller {
+export class InteractionController {
   constructor(private readonly interactionService: InteractionService) {}
 
   @Post()
