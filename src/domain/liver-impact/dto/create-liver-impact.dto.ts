64d269231d381be46f2de479d304a2ababb09680
김하림 <kittyyakki93@gmail.com>
2026-04-05 15:21:09 +0900

    feat:(create liver-impact.dto.ts)

---
+++ src/domain/liver-impact/dto/create-liver-impact.dto.ts
@@ -0,0 +1,20 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsInt, IsString } from "class-validator";
+
+export class CreateLiverImpactDto {
+  @ApiProperty()
+  @IsInt()
+  ingredient_id: number;
+
+  @ApiProperty({ example: 'HIGE' })
+  @IsString()
+  impact_lever: string;
+
+  @ApiProperty()
+  @IsString()
+  description: string;
+
+  @ApiProperty()
+  @IsString()
+  warning_message: string;
+}
