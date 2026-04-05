ade5a1d8a73542b6ded02ae7e9a4715fd1a78ffe
김하림 <kittyyakki93@gmail.com>
2026-04-05 12:04:43 +0900

    feat:(app.module controller import 중복삭제)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,16 @@ import { MemberModule } from './module/member/member.module';
 import { CoreModule } from './module/core/core.module';
 import { AuthModule } from './module/auth/auth.module';
 
+import { IngredientController } from './controller/ingredient/ingredient.controller';
+import { IngredientService } from './service/ingredient/ingredient.service';
+
 @Module({
   imports: [
     CoreModule,
     MemberModule,
     AuthModule,
   ],
-  controllers: [],
-  providers: [],
+  controllers: [IngredientController],
+  providers: [IngredientService],
 })
 export class AppModule {}
