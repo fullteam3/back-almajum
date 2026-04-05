37896eb7166209e3b2cb1ce743b3580438783e0e
김하림 <kittyyakki93@gmail.com>
2026-04-05 16:41:24 +0900

    feat:( fix ingredient.controller.ts )

--- src/controller/ingredient/ingredient.controller.ts
+++ src/controller/ingredient/ingredient.controller.ts
@@ -1,19 +1,20 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { IngredientService } from '../../service/ingredient/ingredient.service';
 import { AnalyzeIngredientDto } from 'src/domain/ingredient/dto/analyze-ingredient.dto';
+import { CreateIngredientDto } from 'src/domain/ingredient/dto/create-ingredient.dto';
 
 
 @Controller('ingredient')
 export class IngredientController {
-  constructor(private readonly ingredientServie: IngredientService) {}
+  constructor(private readonly ingredientService: IngredientService) {}
 
   @Post('analyze')
   analyze(@Body() dto: AnalyzeIngredientDto) {
-    return this.ingredientServie.analyze(dto);
+    return this.ingredientService.analyze(dto);
   }
 
   @Post()
-  create(@Body() dto: CreateIngredientDto)[
-  return this.IngredientService.create(dto);
-  ]
+  create(@Body() dto: CreateIngredientDto) {
+    return this.ingredientService.create(dto);
+  }
 }
