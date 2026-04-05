e376db48988004c4c194ed0fa12b744580a6a25a
김하림 <kittyyakki93@gmail.com>
2026-04-05 13:39:04 +0900

    feat:(create prisma module)

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,15 @@ import { AuthModule } from './module/auth/auth.module';
 
 import { IngredientController } from './controller/ingredient/ingredient.controller';
 import { IngredientService } from './service/ingredient/ingredient.service';
+import { PrismaModule } from './module/prisma/prisma.module';
+
 
 @Module({
   imports: [
     CoreModule,
     MemberModule,
     AuthModule,
+    PrismaModule
   ],
   controllers: [IngredientController],
   providers: [IngredientService],
