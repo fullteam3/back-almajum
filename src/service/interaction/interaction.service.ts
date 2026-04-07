12b6ef6a7fef07046d709120165dc69296ceb77c
김하림 <kittyyakki93@gmail.com>
2026-04-07 10:41:50 +0900

    feat:(Update interaction.service.ts)
    findpair

--- src/service/interaction/interaction.service.ts
+++ src/service/interaction/interaction.service.ts
@@ -1,8 +1,10 @@
 import { Injectable, BadRequestException } from '@nestjs/common';
+import { Interaction } from '@prisma/client';
 
-import { CreateInteractionDto } from 'src/domain/interaction/dto/create-interaction.dto';
+
 import { InteractionRepository } from 'src/repository/interaction/interaction.repository';
 import { PrismaService } from '../prisma/prisma.service';
+import { CreateInteractionDto } from 'src/domain/interaction/dto/create-interaction.dto';
 
 @Injectable()
 export class InteractionService {
@@ -13,22 +15,53 @@ export class InteractionService {
   ) {}
 
   async create(dto: CreateInteractionDto) {
+
+    // 정렬 (핵심)
+    const a = Math.min(dto.ingredient_a_id, dto.ingredient_b_id);
+    const b = Math.max(dto.ingredient_a_id, dto.ingredient_b_id);
+
     const ingredientA = await this.prisma.ingredient.findUnique({
-      where: { id: dto.ingredient_a_id },
+      where: { id: a },
     });
 
     const ingredientB = await this.prisma.ingredient.findUnique({
-      where: { id: dto.ingredient_b_id },
+      where: { id: b },
     });
 
     if (!ingredientA || !ingredientB) {
       throw new BadRequestException('존재하지 않는 성분입니다.');
     }
-
-    return this.interactionRepository.createInteraction(dto);
+  
+    return this.interactionRepository.createInteraction({
+      ...dto,
+      ingredient_a_id: a,
+      ingredient_b_id: b,
+    });
   }
 
+  async findPair(a: number, b: number) {
+    return this.interactionRepository.findPair(a, b);
+  }
   async findAll() {
     return this.interactionRepository.findAll();
+  }
+
+  async checkInteractions(ingredientIds: number[]) {
+    const results: Interaction[] = [];
+  
+    for (let i = 0; i < ingredientIds.length; i++) {
+      for (let j = i + 1; j < ingredientIds.length; j++) {
+        const a = ingredientIds[i];
+        const b = ingredientIds[j];
+  
+        const interaction = await this.findPair(a, b);
+  
+        if (interaction) {
+          results.push(interaction);
+        }
+      }
+    }
+  
+    return results;
   }
 }
