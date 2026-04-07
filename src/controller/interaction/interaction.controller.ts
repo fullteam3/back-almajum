02816ebe652f5aa3357f0d101e8369f545a54e71
김하림 <kittyyakki93@gmail.com>
2026-04-07 10:46:08 +0900

    feat:(Update interaction.controller.ts)

--- src/controller/interaction/interaction.controller.ts
+++ src/controller/interaction/interaction.controller.ts
@@ -1,19 +1,33 @@
-import { Body, Controller, Post, Get } from "@nestjs/common";
-import { CreateInteractionDto } from "src/domain/interaction/dto/create-interaction.dto";
+import { Body, Controller, Post, Get, Query } from "@nestjs/common";
+import { CheckInteractionDto } from "src/domain/check-interaction/dto/check-interaction.dto";
 
+import { CreateInteractionDto } from "src/domain/interaction/dto/create-interaction.dto";
 import { InteractionService } from "src/service/interaction/interaction.service";
 
 @Controller('interaction')
 export class InteractionController {
   constructor(private readonly interactionService: InteractionService) {}
-
+  
   @Post()
   create(@Body() dto: CreateInteractionDto) {
     return this.interactionService.create(dto);
   }
-
+  
   @Get()
   findAll() {
     return this.interactionService.findAll();
   }
+  
+  @Get('pair')
+  findPair(
+    @Query('a') a: string,
+    @Query('b') b: string,
+  ) {
+    return this.interactionService.findPair(Number(a), Number(b));
+  }
+
+  @Post('check')
+check(@Body() dto: CheckInteractionDto) {
+  return this.interactionService.checkInteractions(dto.ingredientIds);
+}
 }
