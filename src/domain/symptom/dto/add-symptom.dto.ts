e2f6c78087ad0da0c8db4a86b14ec68f99e01ff2
김하림 <kittyyakki93@gmail.com>
2026-04-09 15:53:23 +0900

    feat:(Create add-symptom.dto.ts)

---
+++ src/domain/symptom/dto/add-symptom.dto.ts
@@ -0,0 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsArray, ArrayNotEmpty, IsInt } from 'class-validator';
+
+export class AddSymptomDto {
+  @ApiProperty({
+    example: [1],
+    description: '증상 ID 배열',
+  })
+  @IsArray()
+  @ArrayNotEmpty()
+  @IsInt({ each: true })
+  symptomIds: number[];
+}
