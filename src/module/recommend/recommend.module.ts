9c4d82584d79d0a88e9e871dd37ae9879b1d536b
김하림 <kittyyakki93@gmail.com>
2026-04-10 16:47:45 +0900

    feat:(Update recommend.module.ts)

--- src/module/recommend/recommend.module.ts
+++ src/module/recommend/recommend.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RecommendController } from 'src/controller/recommend/recommend.controller';
 import { RecommendService } from 'src/service/recommend/recommend.service';
+import { InteractionModule } from '../interaction/interaction.module';
 
 @Module({
+  imports: [InteractionModule],
   controllers: [RecommendController],
   providers: [RecommendService],
 })
